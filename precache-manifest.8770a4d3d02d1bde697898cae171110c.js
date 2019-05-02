1ba523c99a68181abcfbd1b47c0e601a7402cb66
dinhnguyen3009 <nguyentdinh3009@gmail.com>
2019-05-02 17:22:16 +0700

    Updates

---
+++ precache-manifest.8770a4d3d02d1bde697898cae171110c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95766adb65320595f31b43d76bd52c2a",
+    "url": "/backend-comic/index.html"
+  },
+  {
+    "revision": "d9cdada7617b936a146c",
+    "url": "/backend-comic/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "7e60ae4c3d59ea216be7",
+    "url": "/backend-comic/static/js/2.87b4132c.chunk.js"
+  },
+  {
+    "revision": "d9cdada7617b936a146c",
+    "url": "/backend-comic/static/js/main.439c4da6.chunk.js"
+  },
+  {
+    "revision": "833887c38b6c8c033024",
+    "url": "/backend-comic/static/js/runtime~main.1879767c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/backend-comic/precache-manifest.8770a4d3d02d1bde697898cae171110c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/backend-comic/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
